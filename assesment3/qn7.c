1d383cf9e444840f2891b42223c4563ec951caec
Gowtham-k-407 <kanagarajgowtham8@gmail.com>
2026-01-29 09:33:40 +0530

    Assesment 3

---
+++ assesment3/qn7.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+
+int main() {
+    int a;
+    scanf("%d",&a);
+    int d0=a/10, d1=a%10;
+    if(d0==d1){
+        printf("Success");
+    }
+    else{
+        printf("Failure");
+    }
+    
+     return 0;
+}
